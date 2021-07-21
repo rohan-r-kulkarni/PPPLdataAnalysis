318fb8766575699ee316b724d502404f91e57b09
Hantao Ji <hji@Hantaos-MBP.hsd1.nj.comcast.net>
2021-07-21 11:30:11 -0400

    Changed hi to hello

--- fields.py
+++ fields.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-print("hi")
+print("Hello")
 
 filename = "./20170810data/mms1/edp/mms1_edp_brst_l2_scpot_20170810121733_v2.4.0.cdf"
 file = cdflib.CDF(filename)
